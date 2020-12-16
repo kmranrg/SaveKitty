d48bbbb9b8c2b92de2b157d2f6a1a22f4c8a4508
kmranrg <kmranrg@gmail.com>
2020-12-16 18:13:58 +0530

    unity state machine part 2

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,19 +6,23 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
 	[SerializeField] float runSpeed = 5f;
+	[SerializeField] float jumpSpeed = 5f;
 	Rigidbody2D playerRigidBody;
 	Animator playerAnimator;
+	CapsuleCollider2D feetCollider;
 
 	// Use this for initialization
 	void Start () {
 		playerRigidBody = GetComponent<Rigidbody2D> ();
-		playerAnimator = GetComponent<Animator>();
+		playerAnimator = GetComponent<Animator> ();
+		feetCollider = GetComponent<CapsuleCollider2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Run();
 		FlipSprite();
+		Jump();
 	}
 
 	private void Run() {
@@ -28,6 +32,19 @@ public class Player : MonoBehaviour {
 
 		bool isPlayerMoving = Mathf.Abs (playerRigidBody.velocity.x) > 0;
 		playerAnimator.SetBool ("isWalking", isPlayerMoving);
+	}
+
+	private void Jump() {
+		if(!feetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+			playerAnimator.SetBool("isJumping", false);
+			return;
+		}
+
+		if (CrossPlatformInputManager.GetButtonDown("Jump")) {
+			Vector2 jumpVelocity = new Vector2 (0f, jumpSpeed);
+			playerRigidBody.velocity += jumpVelocity;
+			playerAnimator.SetBool("isJumping", true);
+		}
 	}
 
 	private void FlipSprite() {
