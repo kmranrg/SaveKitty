a4b62f151760028d006a50d6cb593053e23ddde7
kmranrg <kmranrg@gmail.com>
2020-12-18 18:04:11 +0530

    collectables

---
+++ Assets/Script/Collectables.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    [SerializeField] int spiritValue = 1;
+    private void OnTriggerEnter2D(Collider2D other) {
+        FindObjectOfType<GameSession>().IncreaseScore(spiritValue);
+        Destroy (gameObject);    
+    }
+}
---
+++ Assets/Script/GameSession.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int spiritScore = 0;
+    [SerializeField] Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = spiritScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void IncreaseScore (int pointsToAdd) {
+        spiritScore += pointsToAdd;
+        scoreText.text = spiritScore.ToString();
+    }
+    
+}
