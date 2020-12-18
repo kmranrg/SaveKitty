8c6810d7d99133eb420e0acdb47f0fe857216467
kmranrg <kmranrg@gmail.com>
2020-12-18 20:15:14 +0530

    game session part 2

--- Assets/Script/GameSession.cs
+++ Assets/Script/GameSession.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int spiritScore = 0;
     [SerializeField] Text scoreText;
+
+    [SerializeField] int playerMaxHearts = 5;
+    [SerializeField] int playerLives = 3;
+
+    [SerializeField] Image[] hearts;
+    [SerializeField] Sprite fullHeart;
+    [SerializeField] Sprite emptyHeart;
 
     void Awake()
     {
@@ -27,12 +35,48 @@ public class GameSession : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (playerLives > playerMaxHearts) {
+            playerLives = playerMaxHearts;
+        }
 
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < playerMaxHearts) {
+                hearts[i].enabled = true;
+            } else {
+                hearts[i].enabled = false;
+            }
+
+            if (i < playerLives) {
+                hearts[i].sprite = fullHeart;
+            } else {
+                hearts[i].sprite = emptyHeart;
+            }
+        }
     }
 
     public void IncreaseScore (int pointsToAdd) {
         spiritScore += pointsToAdd;
         scoreText.text = spiritScore.ToString();
+    }
+
+    public void ProcessPlayerDeath() {
+        if(playerLives > 1) {
+            RemoveLives();
+        } else {
+            ResetGame();
+        }
+    }
+
+    public void RemoveLives() {
+        playerLives--;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void ResetGame() {
+        SceneManager.LoadScene(1); // `1` index is here for Main Menu
+        Destroy(gameObject);
     }
     
 }
