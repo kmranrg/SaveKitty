72444cf8544d6cd66cf2a2dca07abd98537523c4
kmranrg <kmranrg@gmail.com>
2020-12-19 18:26:19 +0530

    fixed kappa legs

--- Assets/Script/GateLevel3.cs
+++ Assets/Script/GateLevel3.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Threading;
 
 public class GateLevel3 : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other) {
-        SceneManager.LoadScene(1); // `1` index is here for Main Menu
-        Destroy(gameObject);
+        FindObjectOfType<GameSession>().ResetGame();
     }
 }
