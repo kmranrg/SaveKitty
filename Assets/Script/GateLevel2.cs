9d7beca1bed14f5a4ae9c37da0c6f0b21ed6cf58
kmranrg <kmranrg@gmail.com>
2020-12-19 16:15:30 +0530

    game version 1.0 published

---
+++ Assets/Script/GateLevel2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
+public class GateLevel2 : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other) {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene( currentSceneIndex + 1 );
+    }
+}
---
+++ Assets/Script/GateLevel3.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
+public class GateLevel3 : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other) {
+        SceneManager.LoadScene(1); // `1` index is here for Main Menu
+        Destroy(gameObject);
+    }
+}
